c42a4d12366e46b8000bd81aad1d776c63e9dd07
dcoollx <dsqjr@msn.com>
2026-06-11 19:44:30 -0400

    cta animation

--- apps/frontend/src/components/CTA.tsx
+++ apps/frontend/src/components/CTA.tsx
@@ -2,8 +2,26 @@ import { motion } from "motion/react";
 
 export const CTA = () => {
     return (
-        <motion.div className="sticky bottom-0 left-0 right-0 bg-brand text-black font-bold uppercase tracking-wider text-center py-4 z-50">
-        Are you looking to book new events at your venue?
+        <motion.div
+        initial={{
+            height: "0px",
+            opacity: 0,
+        }}
+        exit={{
+            height: 0,
+            opacity: 0,
+        }}
+         animate="visible"
+        variants={{
+            hidden: { height: "0px", opacity: 0 },
+            visible: { height: "3%", opacity: 1 },
+        }}
+        transition={{
+            duration: 0.5,
+            delay: 1,
+        }}
+         className="sticky bottom-0 left-0 right-0 bg-brand text-black font-bold uppercase tracking-wider text-center py-4 z-50">
+        Are you looking to book new events at your venue? <a href="mailto:events@lovewithlegs.com" className="text-black underline">Contact us</a>
         </motion.div>
     );
 }
