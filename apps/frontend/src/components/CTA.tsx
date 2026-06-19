68fc30057a9392c30e3675fe2ed6230eb7e81951
dcoollx <dsqjr@msn.com>
2026-06-19 19:45:55 -0400

    cta fixed

--- apps/frontend/src/components/CTA.tsx
+++ apps/frontend/src/components/CTA.tsx
@@ -22,12 +22,17 @@ export const CTA = ({ visible }: { visible: boolean })=> {
         }}
          onMouseEnter={() => setIsHovered(true)}
          onMouseLeave={() => setIsHovered(false)}
-         className=" fixed w-full bg-triadic text-white font-bold  tracking-wider text-center py-4 z-50">
-        <span className="text-lg uppercase">Are you looking to book new events at your venue? </span>
-        <div className={` mt-10 mx-auto w-250 flex  flex-col ${isHovered ? '' : 'hidden'}`}>
-            <div className="text-lg flex-row">Coach Classic is the perfect solution for your next event. Contact Me Today to learn more!</div>
-            <a className="bold border-white border-2 mt-10 p-10 text-white hover:underline" href="mailto:events@lovewithlegs.com?subject=Book Coach Classic at My Venue">Contact us</a>
+         className="fixed w-full bg-slate-700  text-white font-bold  tracking-wider text-center py-0 z-50">
+             
+        <div className="bg-brand w-full h-16 border border-white">
+        <span className="text-lg leading-[64px] uppercase">Are you looking to book new events at your venue? </span>
         </div>
+        <div className={`sm:px-4 mt-10 mx-auto w-250 flex  flex-col ${isHovered ? '' : 'hidden'}`}>
+            <div className="text-lg flex-row text-left">Coach Classic brings unmatched energy, professionalism, and community spirit to every gathering, making him the ideal choice for your next wellness event, corporate activity, or group fitness session. With engaging classes designed for all ages and fitness levels, he transforms local parks and recreation centers into vibrant spaces where people move, laugh, and feel inspired. Whether you're planning a team‑building day, a community celebration, or a special wellness initiative, Coach Classic delivers an experience that leaves everyone feeling stronger and more connected.</div>
+            <a className=" hover:bg-brand active:bg-active flex-shrink border-white border-2 mt-10 p-4 text-white hover:underline" href="mailto:events@lovewithlegs.com?subject=Book Coach Classic at My Venue">Contact Me Today</a>
+        </div>
+
+
        </motion.div>
     );
 }
